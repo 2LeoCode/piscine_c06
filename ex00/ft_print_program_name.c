00a4aba56125fdb1050e8a38b82aec45b131ead0
Your Name <you@example.co>
2020-02-13 17:23:05 +0100

    blob

---
+++ ex00/ft_print_program_name.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsuardi <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/13 11:57:38 by lsuardi           #+#    #+#             */
+/*   Updated: 2020/02/13 12:26:55 by lsuardi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str);
+
+int		main(int argc, char **argv)
+{
+	ft_putstr(argv[0]);
+	return (0);
+}
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = -1;
+	while (str[++i])
+		write(1, &str[i], 1);
+	write(1, "\n", 1);
+}
